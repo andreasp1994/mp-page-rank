a5e61f040489c2840ee322e3fd5bf7740387f304
2140845p <andreasp1994@gmail.com>
2018-02-12 04:38:34 -0800

    handling outgoing links

--- src/main/java/pagerank/PageRankMapper.java
+++ src/main/java/pagerank/PageRankMapper.java
@@ -14,20 +14,21 @@ public class PageRankMapper extends Mapper<Text, Text, Text, Text> {
 	
 	@Override
 	protected void map(Text key, Text pageRankAndLinks, Context context) throws IOException, InterruptedException {
-		String linksOut = pageRankAndLinks.toString().split("###")[1];
-		String[] linksOutArr = linksOut.split(" ");
-		String pageRank = pageRankAndLinks.toString().split("###")[0];
-		int linksOutLength = linksOutArr.length;
-		
-		for(String article : linksOutArr) {
-			context.write(new Text(article), new Text(pageRank + "###" + linksOutLength + "###" + key.toString()));
+		String[] splittedValues = pageRankAndLinks.toString().split("###");
+		if (splittedValues.length > 1) {	// Has outgoing links.
+			String pageRank = splittedValues[0];
+			String linksOut = splittedValues[1];
+			String[] linksOutArr = linksOut.split(" ");
+			int linksOutLength = linksOutArr.length;
+			for(String article : linksOutArr) {
+				context.write(new Text(article), new Text(pageRank + "###" + linksOutLength + "###" + key.toString()));
+			}
+			context.write(key, new Text("$LINKSOUT$" + linksOut));
 		}
-		context.write(key, new Text("$LINKSOUT$" + linksOut));
 	}
 	
 	@Override
 	protected void cleanup(Context context) throws IOException, InterruptedException {
-		// ...
 		super.cleanup(context);
 	}
 }
